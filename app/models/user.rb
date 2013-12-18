1d7fbc385fe727da4c91b58efff3bc99c75e716b
Daniel Walsh <walshd15@ytcd.ie>
2013-12-18 01:15:08 +0000

    Finish User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	#alternately: before_save { email.downcase! }
 	validates :name, presence: true, length: {maximum: 50}
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	#REGEX =regular expression, Ruby constants start with a capital letter by convention
 	#More on regular expressions: rubular regular expression editor
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
